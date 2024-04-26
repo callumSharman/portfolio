8387fa1f68bc5989559e4f2daf0c6880b80cd982
lettussandwich <csharman@student.unimelb.edu.au>
2024-04-26 17:52:10 +1000

    rough site mockup begun

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [sideMenuWidth, setSideMenuWidth] = useState("");
+  const [sideMenuZIndex, setSideMenuZIndex] = useState("");
+
+
+  function onMenuClick(){
+    setSideMenuWidth("66vw");
+    setSideMenuZIndex("100");
+  }
+
   return (
+  <>
+    <div className='sideMenu' style={{width:sideMenuWidth, zIndex:sideMenuZIndex}}>
+      <p></p>
+
+      <div className='menuButton' onClick={onMenuClick}>
+        =
+
+      </div>
+
+    </div>
     <div className="App">
       <div className="appContents">
         <h1>Callum Sharman</h1>
         <p>
           -
           <br></br>
-          An enthusiastic and dedicated Computer Science graduate from The University of Melbourne with strong teamwork, 
-          leadership, and communication skills, seeking an internship, graduate role, or entry-level position. 
-          Driven individual, adept in a variety of programming languages, Node.js and the Django web development framework. 
-          Experienced in utilising agile ceremonies to enable smooth development in teams.
+          Consequat laborum ea reprehenderit consequat officia ea. Ad dolore ipsum proident Lorem. Qui incididunt laborum duis enim Lorem cillum Lorem laboris. Irure commodo velit voluptate qui ea anim tempor ex reprehenderit culpa. Est nulla culpa esse do aliqua incididunt aliqua anim.
+          Cillum cillum id mollit elit commodo labore pariatur enim ex esse non ipsum fugiat. Qui laboris cupidatat consequat sit pariatur nulla ex cupidatat eu mollit anim elit. Tempor cillum elit laborum do aute sunt fugiat. Fugiat reprehenderit duis adipisicing sit commodo ut sit cupidatat consequat ullamco ullamco Lorem magna cupidatat. Nostrud aute excepteur anim adipisicing Lorem cillum laborum. Ea nisi esse et consectetur qui consequat tempor amet laboris officia elit veniam eu ea.
           <br></br>
           -
         </p>
 
+        <div className='proj1'>
+          Project1
+        </div>
+
+        <div className='proj2'>
+          Project2
+        </div>
       </div>
     </div>
+
+    <div className='footer'>
+
+    </div>
+  </>
   );
 }
 
