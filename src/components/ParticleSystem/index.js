37e1fc1e97522c244f48a02df49dc61450e914e4
lettussandwich <csharman@student.unimelb.edu.au>
2024-05-09 17:13:04 +1000

    particle lifetime added

--- src/components/ParticleSystem/index.js
+++ src/components/ParticleSystem/index.js
@@ -11,9 +11,8 @@ function ParticleSystem(props){
     ctx.fillStyle = '#171a1f';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    let circles = initCircleList(100, canvas.width, canvas.height, 4);
-    animateCanvas(circles, canvas);
-
+    let circles = initCircleList(100, canvas.width, canvas.height, 4, 500);
+    animateCanvas(circles, canvas, 16);
   })
 
 
@@ -28,20 +27,23 @@ export default ParticleSystem;
 /**
  * generates a list of n cirlces centered randomly within a rectangle
  * of 'width' and 'height' with a random radius up to 'maxR'
- * @param {number} n number of circles
- * @param {number} width of area 
- * @param {number} height of area
- * @param {number} maxR max radius
- * @returns {Array<{ x: number, y: number, r: number }>}
+ * @param {number} n - Number of circles
+ * @param {number} width - of area 
+ * @param {number} height - of area
+ * @param {number} maxRadius
+ * @param {number} maxLifeTime - Maximum time in frames for particle life
+ * @returns {Array<{ x: number, y: number, r: number, t: number }>} - 't' is time remaining
  */
-function initCircleList(n, width, height, maxR){
+function initCircleList(n, width, height, maxRadius, maxLifeTime){
   let circles = [];
 
   for(let i = 0; i < n; i ++){
     circles.push({
       x: Math.random() * width,
       y: Math.random() * height,
-      r: Math.random() * maxR});
+      r: Math.random() * maxRadius,
+      t: Math.random() * maxLifeTime,
+    });
   }
 
   return circles;
@@ -49,7 +51,7 @@ function initCircleList(n, width, height, maxR){
 
 /**
  * draws a list of circles to the given canvas context
- * @param {Array<{ x: number, y: number, r: number }>} circles list of circles
+ * @param {Array<{ x: number, y: number, r: number, t: number }>} circles - List of circles
  * @param {HTMLCanvasElement} canvas
  */
 function drawCircles(circles, canvas){
@@ -64,18 +66,26 @@ function drawCircles(circles, canvas){
 }
 
 /**
- * updates a list of cirlces based on some rules
- * @param {Array<{ x: number, y: number, r: number }>} circles 
+ * updates a list of cirlces based on some rules and returns the updated list
+ * @param {Array<{ x: number, y: number, r: number, t: number }>} circles 
+ * @returns {Array<{ x: number, y: number, r: number, t: number }>}
  */
 function updateCircles(circles){
+
+  // remove dead circles from the list
+  circles = circles.filter(circle => circle.t >= 0);
+
   circles.forEach(circle => {
-    circle.x -= 10;
+    circle.x -= 1;
+    circle.t -= 1;
   });
+
+  return circles
 }
 
 /**
  * 
- * @param {Array<{ x: number, y: number, r: number }>} circles 
+ * @param {Array<{ x: number, y: number, r: number, t: number }>} circles 
  * @param {HTMLCanvasElement} canvas 
  */
 function animateCanvas(circles, canvas){
@@ -84,9 +94,14 @@ function animateCanvas(circles, canvas){
   const ctx = canvas.getContext('2d');
   ctx.clearRect(0,0, canvas.width, canvas.height);
 
-  updateCircles(circles);
+  circles = updateCircles(circles);
   drawCircles(circles, canvas);
 
   // recursively loop through itself to animate
-  window.requestAnimationFrame(() => animateCanvas(circles, canvas))
+
+  // tied to framerate!!
+  window.requestAnimationFrame(() => animateCanvas(circles, canvas));
+
+  // not tied to framerate BUT extremely jittery (interval is in milliseconds)
+  //setTimeout(() => animateCanvas(circles, canvas, interval), interval);
 }
