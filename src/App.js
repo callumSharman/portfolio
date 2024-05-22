69952a69a8f985aa5d57c1df1a536c8fd634f3c8
lettussandwich <csharman@student.unimelb.edu.au>
2024-05-22 14:08:07 +1000

    Temporary about section added

--- src/App.js
+++ src/App.js
@@ -13,24 +13,35 @@ function App() {
   return (
   <div className="App">
       <Menu sections={ sections }/>
-      <ParticleSystem width={windowWidth} height={windowHeight}/>
+      {/* <ParticleSystem width={windowWidth} height={windowHeight}/> */}
       <div className='main'>
         <div className='mainContent'>
           <h1>CALLUM SHARMAN</h1>
-          <h2>&lt; Graduate Software Engineer/Computer Scientist /&gt;</h2>
-          Do fugiat ut cupidatat pariatur occaecat dolor ipsum officia anim tempor. Anim officia sint sit laboris. Ipsum enim nostrud officia sit fugiat qui elit laborum duis aliquip est do magna ullamco. Excepteur aliquip mollit enim cillum ad veniam commodo minim deserunt. Do excepteur sint proident aliquip officia exercitation minim quis aliqua adipisicing officia aute. Consequat exercitation anim Lorem consequat magna nulla minim excepteur aliquip.
+          <h2>&lt; Graduate Software Engineer - Computer Scientist - Freelancer /&gt;</h2>
+          Concise description of me here
           <div className='section' id='about'>
             <h2>ABOUT</h2>
-            Voluptate mollit deserunt irure dolor mollit officia et id nisi tempor aliquip do cupidatat do. Sunt elit deserunt ipsum nisi reprehenderit do quis veniam laboris voluptate reprehenderit. Incididunt proident nostrud laboris sit occaecat cupidatat.</div>
+            I'm a recent graduate from the University of Melbourne, holding a Bachelor of Science in Computing and Software Systems. I thrive on tackling complex projects and 
+            delivering innovative solutions.
+            <br></br><br></br>
+            My experience includes full-stack development using Django and Node.js, as well as creating custom web applications that meet client needs. I’ve successfully managed 
+            projects from start to finish, ensuring clear communication and adherence to client requirements. 
+            <br></br><br></br>
+            My passion for technology drives me to continually learn and grow, making meaningful contributions in fast-paced environments. 
+            I am excited to bring my diverse skill set and enthusiasm for problem-solving to new challenges.
+          </div>
           <div className='section' id='projects'>
             <h2>PROJECTS</h2>
-            Magna quis Lorem deserunt ut duis eiusmod incididunt elit cupidatat aliquip. Aliquip incididunt ullamco ullamco do eu dolore nostrud nulla voluptate dolor adipisicing nostrud. Nulla elit Lorem irure aliqua consectetur ullamco.</div>
+            Magna quis Lorem deserunt ut duis eiusmod incididunt elit cupidatat aliquip. Aliquip incididunt ullamco ullamco do eu dolore nostrud nulla voluptate dolor adipisicing nostrud. Nulla elit Lorem irure aliqua consectetur ullamco.
+          </div>
           <div className='section' id='skills'>
             <h2>SKILLS</h2>
-            Do et eiusmod ex sunt dolore nostrud. Minim est cupidatat dolor consectetur ex occaecat dolore Lorem officia ex do nisi adipisicing. Qui laborum esse ad ipsum dolor sint excepteur elit veniam aute cillum duis ex excepteur. Nisi occaecat laborum eiusmod laborum fugiat velit aute aliquip excepteur officia aliquip deserunt cillum eiusmod. Sunt esse consequat eiusmod adipisicing laborum labore ea.</div>
+            Do et eiusmod ex sunt dolore nostrud. Minim est cupidatat dolor consectetur ex occaecat dolore Lorem officia ex do nisi adipisicing. Qui laborum esse ad ipsum dolor sint excepteur elit veniam aute cillum duis ex excepteur. Nisi occaecat laborum eiusmod laborum fugiat velit aute aliquip excepteur officia aliquip deserunt cillum eiusmod. Sunt esse consequat eiusmod adipisicing laborum labore ea.
+          </div>
           <div className='section' id='resume'>
             <h2>RESUME</h2>
-            Ut et labore fugiat est tempor laborum exercitation. Reprehenderit eu velit anim do duis eiusmod nisi duis tempor anim cillum labore officia velit. Quis ullamco do nostrud laborum amet excepteur eu Lorem reprehenderit elit pariatur duis. Ad consequat Lorem ullamco aliquip aliquip nulla occaecat elit. Magna et commodo ut laborum qui esse occaecat proident laborum et sunt laboris culpa.</div>
+            Ut et labore fugiat est tempor laborum exercitation. Reprehenderit eu velit anim do duis eiusmod nisi duis tempor anim cillum labore officia velit. Quis ullamco do nostrud laborum amet excepteur eu Lorem reprehenderit elit pariatur duis. Ad consequat Lorem ullamco aliquip aliquip nulla occaecat elit. Magna et commodo ut laborum qui esse occaecat proident laborum et sunt laboris culpa.
+          </div>
         </div>
       </div>
   </div>
