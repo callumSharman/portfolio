bdd0a68a7c6231b7351fa13a8d95ae7d14fd76c7
lettussandwich <csharman@student.unimelb.edu.au>
2024-05-22 14:47:37 +1000

    Logos and links added to menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,7 @@
 import './index.css'
+import linkedInLogo from '../../img/icons8-linkedin.svg';
+import gitHubLogo from '../../img/icons8-github.svg';
+import goodreadsLogo from '../../img/icons8-goodreads.svg';
 
 function Menu({ sections }){
   function scrollToSection(id){
@@ -23,9 +26,21 @@ function Menu({ sections }){
 
         <div className='externalLinks'>
           {/* links to socials */}
-          <div className='externalLink'></div>
-          <div className='externalLink'></div>
-          <div className='externalLink'></div>
+          <div className='externalLink'>
+            <a href="https://www.linkedin.com/in/callum-sharman-366b532ab/" target="_blank" rel="noopener noreferrer">
+              <img className='logo' src={linkedInLogo} alt="LinkedIn Logo"/>
+            </a>
+          </div>
+          <div className='externalLink'>
+            <a href="https://github.com/callumSharman" target="_blank" rel="noopener noreferrer">
+              <img className='logo' src={gitHubLogo} alt="GitHub Logo"/>
+            </a>
+          </div>
+          <div className='externalLink'>
+            <a href="https://www.goodreads.com/user/show/165214915-callum-sharman" target="_blank" rel="noopener noreferrer">
+              <img className='logo' src={goodreadsLogo} alt="Goodreads Logo"/>
+            </a>
+          </div>
         </div>
       </div>
     </>
