71f4511d26c82eaf87602f9c5ecda6409c2bd40f
lettussandwich <csharman@student.unimelb.edu.au>
2024-06-06 16:47:31 +1000

    menu transitions and button working

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,14 +1,16 @@
 import './index.css'
 import SocialLinks from '../SocialLinks/index.js'
+import { useState } from 'react';
 
 function Menu({ sections }){
+  const [inMobileMode, setInMobileMode] = useState(false);
+  const [mobileMenuOpen, setMoblieMenuOpen] = useState(true);
 
   function scrollToSection(id){
     document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
 
   }
 
-  // convert the given sections in html
   const links = sections.map((section, index) => {
     return<li key={index} 
               style={{fontSize: section.active ? '20pt': '12pt',
@@ -22,7 +24,19 @@ function Menu({ sections }){
 
   return(
     <>
-      <div className='menu'>
+      <div className='menu' style={{height:mobileMenuOpen ? '100%':'60px'}}>
+        <div style={{display:inMobileMode ? 'block':'none'}}>
+          <div className='menuButton' onClick={ () => setMoblieMenuOpen(!mobileMenuOpen) }
+            style={{display:mobileMenuOpen ? 'flex': 'none',}}>
+            X
+          </div>
+          <div className='menuButton' onClick={ () => setMoblieMenuOpen(!mobileMenuOpen) }
+            style={{display:mobileMenuOpen ? 'none': 'flex',}}>
+            <div>
+              <div className="menuBtnLine"></div><div className="menuBtnLine"></div><div className="menuBtnLine"></div>
+            </div>
+          </div>
+        </div>
 
         <div className='inPageLinks'>
 
